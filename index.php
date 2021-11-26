792dbb661ae10e57c3e6c079260c27d705413828
PaulG0 <paul.gouin@sts-sio-caen.info>
2021-11-26 14:35:18 +0100

    menu v1

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 
 
 <?php
-$nav=file_get_contents('data/nav.yaml');
-$page=yaml_parse($contents)
+$content=file_get_contents('data/nav.yaml');
+$page=yaml_parse($content)
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 </head>
 <body>
 	<nav>
-		
+		<nav ></nav>
 		<?php
 		foreach ($page as $nav => $ancre) {
-			echo "<a href="#$ancre"></a>;"
+			echo "<a class='menu'href='#$ancre'>$nav</a>";
 		}
 		?>
 	</nav>
