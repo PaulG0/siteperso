18a6e29c185d048e5608bc51c1140ae249d3bfb1
PaulG0 <paul.gouin@sts-sio-caen.info>
2021-12-14 08:51:36 +0100

    Update propos.php

--- php/propos.php
+++ php/propos.php
@@ -1,6 +1,6 @@
 
 <img src="img/profil.jpg" >
 je m'appelle <?=$persoinfo['prenom']?> <?=$persoinfo['nom']?><br>
-je suis actuellement en etude à <?=$persoinfo['etude']?>en <?=$persoinfo['formation']?> <br>
+je suis actuellement en etude à <?=$persoinfo['etude']?> en <?=$persoinfo['formation']?> <br>
 avec la specialité <?=$persoinfo['specialite']?><br>
 dans <?=$persoinfo['localisation']?>
