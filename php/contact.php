cbfeed52ff4bffdec5e9dad69ce91eebbc539251
PaulG0 <paul.gouin@sts-sio-caen.info>
2021-12-16 21:29:17 +0100

    maj

--- php/contact.php
+++ php/contact.php
@@ -8,19 +8,25 @@
     code postal: <?=$persoinfo['codepostal']?><br>
 
 </fieldset>
-<fieldset>
+<script src="https://www.recaptcha.net/recaptcha/api.js" async defer></script>
+<fieldset class='basfield'>
     <legend>formulaire de contact</legend>
-<form method="post" action="php/mail.php">
-    <label> nom de l'entreprise</label> <br>
-    <input type="text" name="entreprise" require placeholder="nom de l'entreprise"><br>
-    <label > email</label><br>
- 	<input type="email" name="email" require placeholder="Email "/><br>
-    <label>numero de telephonne</label><br>
-	<input type="numero" name="tel" maxlength="10" placeholder="numero de telephone" /><br>
-    <label> informations divers</label><br>
-    <textarea name="body" placeholder="informations supplementaire que vous pouvez souhiaitez en plus">	</textarea>
-    <br>
-	<input type="submit" value="Submit" />
-</form>
+    <form method="post" action="php/mail.php">
+        <label> nom de l'entreprise</label> <br>
+        <input type="text" name="entreprise" required placeholder="nom de l'entreprise"><br>
+        <label > email</label><br>
+        <input type="email" name="email" required placeholder="Email "/><br>
+        <label>numero de telephonne</label><br>
+        <input type="numero" name="tel" maxlength="10" placeholder="numero de telephone" /><br>
+        <label> informations divers</label><br>
+        <textarea name="body" placeholder="informations supplementaire que vous pouvez souhiaitez en plus">	</textarea>
+        <br>
+        <br>
+        <input type="submit" value="Submit" />
+    </form>
 </fieldset>
 
+<!--client-->
+6LdkZKkdAAAAAKgMLMlShdyOvZlJjJfZeODPa8mU
+<!--serveur-->
+6LdkZKkdAAAAAOW1LOAb1dThGCyif1DUf4RwAcSN
